7f2a7b0033ee54cb63e4293940b87b60903581ac
石黒　友哉 <ishiguro-tomoya@ishigurarinoMBP.lan>
2020-05-19 13:15:27 +0900

    [Start]5-4

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '496bb641c81fa6202baea87ad26c9bc40913ac9bebf34c43a9de1701fcca07ccd731d51dca642ed2a9262d252d9d17f3d2e17b63e3f6afafeef76ba2c3828e1c'
